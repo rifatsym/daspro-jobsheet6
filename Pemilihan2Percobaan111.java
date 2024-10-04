8e0a5d097ec24582ee8d58c39047a29a94ac0f2a
rifat syamlan <rifatsyamlan78@gmail.com>
2024-10-04 16:21:51 +0700

    commit kedua

--- Pemilihan2Percobaan111.java
+++ Pemilihan2Percobaan111.java
@@ -8,9 +8,9 @@ public class Pemilihan2Percobaan111 {
         System.out.print("Silahkan Input Tahun: ");
         tahun = input11.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) == 0) {
-                if ((tahun % 400) == 0) {
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
                     System.out.println("Tahun Kabisat");
                 } else {
                     System.out.println("Bukan Tahun Kabisat");
